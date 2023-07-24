e436b7feeb51ed2b322c8a0c5db139305699c932
Manoj Saini <manojsaini513@gmail.com>
2023-07-24 14:30:06 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,38 @@
 //your code here
+const images = document.querySelectorAll('.image');
+let activeImage = null;
+let initialX = 0;
+let initialY = 0;
+let xOffset = 0;
+let yOffset = 0;
+
+images.forEach(image => {
+  image.addEventListener('mousedown', dragStart);
+  image.addEventListener('mouseup', dragEnd);
+  image.addEventListener('mousemove', drag);
+});
+
+function dragStart(event) {
+  activeImage = this;
+  initialX = event.clientX - xOffset;
+  initialY = event.clientY - yOffset;
+}
+
+function dragEnd(event) {
+  activeImage = null;
+  initialX = xOffset;
+  initialY = yOffset;
+}
+
+function drag(event) {
+  if (activeImage) {
+    event.preventDefault();
+    xOffset = event.clientX - initialX;
+    yOffset = event.clientY - initialY;
+    setTranslate(xOffset, yOffset, activeImage);
+  }
+}
+
+function setTranslate(xPos, yPos, el) {
+  el.style.transform = `translate3d(${xPos}px, ${yPos}px, 0)`;
+}
